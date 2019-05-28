c4fb7ec51e2d59474c1c3a856b7f01983513d6f7
Jonathan <jonathan.pannetier@gmail.com>
2019-05-28 18:56:26 +0200

    Create dependencies/configs constants

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,10 @@ buildscript {
     repositories {
         google()
         jcenter()
-        
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
